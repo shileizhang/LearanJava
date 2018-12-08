b82c8d60914cab843693efe9ae4b9b8147dcf0eb
shileizhang <slhhzhang@foxmail.com>
2018-12-08 21:07:26 +0800

    Commit first Test

---
+++ SE/src/com/hdu/learn/day1/Main.java
@@ -0,0 +1,7 @@
+package com.hdu.learn.day1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello！");
+    }
+}
---
+++ SE/src/com/hdu/test/testAll.java
@@ -0,0 +1,4 @@
+package com.hdu.test;
+
+public class testAll {
+}
