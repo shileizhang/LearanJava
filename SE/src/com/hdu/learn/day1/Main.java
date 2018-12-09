a67f90c6be0722ee0ddb9519169155002efd345a
shileizhang <slhhzhang@foxmail.com>
2018-12-09 16:06:48 +0800

    Commit Main 1s out date

--- SE/src/com/hdu/learn/day1/Main.java
+++ SE/src/com/hdu/learn/day1/Main.java
@@ -1,12 +1,16 @@
 package com.hdu.learn.day1;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello！");
-        System.out.println("2");
-        System.out.println("123");
-
-        int x=1;
-        System.out.println(x);
+        for (int i = 0; i < 100; i++) {
+            System.out.println(new Date());
+            try{
+                Thread.sleep(1000);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
