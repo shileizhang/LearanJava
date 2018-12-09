afd5f3c153f5cfd7117d1d7f8ba2134dfc2ce43e
shileizhang <slhhzhang@foxmail.com>
2018-12-09 15:56:55 +0800

    Commit Test2

--- SE/src/com/hdu/learn/day1/Main.java
+++ SE/src/com/hdu/learn/day1/Main.java
@@ -3,5 +3,10 @@ package com.hdu.learn.day1;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello！");
+        System.out.println("2");
+        System.out.println("123");
+
+        int x=1;
+        System.out.println(x);
     }
 }
